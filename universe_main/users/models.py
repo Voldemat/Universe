93435b89890ec376357493060243e39c38de47b9
Voldemat <77781574+Voldemat@users.noreply.github.com>
2021-04-19 23:24:34 +0300

    '04.19.2021'

--- universe_main/users/models.py
+++ universe_main/users/models.py
@@ -1,15 +1,18 @@
 """
     User global model
 """
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 from .validators import max_image_size
 
+
 class UserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, birth_date, avatar, about_me, password = None):
+    def create_user(self, email:str = None, first_name:str = None, last_name:str = None, birth_date:str = None, avatar:object = None, about_me:str = None, password:str = None):
         if not email:
             raise ValueError('Users must have an email address')
+
 
         user = self.model(
             email = self.normalize_email(email),
@@ -24,6 +27,22 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password = None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.create_user(email, password)
+
+
+        user.is_admin = True
+        
+        user.is_staff = True
+
+
+        user.save(using = self._db)
+
+        return user
+
 
 
 
@@ -34,21 +53,25 @@ class User(AbstractBaseUser):
         max_length      = 255,
         unique          = True
     )
-    first_name  = models.CharField('First name', max_length = 255, blank = True)
+    first_name  = models.CharField('First name', max_length = 255, blank = True, null = True)
 
-    last_name   = models.CharField('Last name', max_length = 255, blank = True)
+    last_name   = models.CharField('Last name', max_length = 255, blank = True, null = True)
 
-    birth_date  = models.DateField('Birth date', blank = True)
+    birth_date  = models.DateField('Birth date', blank = True, null = True)
 
     avatar      = models.ImageField(
         verbose_name    = 'Avatar',
         upload_to       = 'users/avatars/',
         validators      = [max_image_size(300,300),],
-        blank           = True
+        blank           = True,
+        null            = True
     )
 
-    about_me    = models.CharField('About me', max_length = 1000, blank = True)
+    about_me    = models.CharField('About me', max_length = 1000, blank = True, null = True)
 
     is_active   = models.BooleanField(default = True)
     is_staff    = models.BooleanField(default = False)
     is_admin    = models.BooleanField(default = False)
+
+
+    objects = UserManager()
