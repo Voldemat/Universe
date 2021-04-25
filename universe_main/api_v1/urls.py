e568bcecdceb0a40f94fe5365cf83116153af800
Voldemat <77781574+Voldemat@users.noreply.github.com>
2021-04-25 22:06:07 +0300

    'add_user_viewset'

--- universe_main/api_v1/urls.py
+++ universe_main/api_v1/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
+from rest_framework.routers import SimpleRouter
+
 from rest_framework_swagger.views import get_swagger_view
+
+from api_v1.views import UserViewSet
+
+router = SimpleRouter()
+router.register('users', UserViewSet, basename = 'users')
 
 swagger_view = get_swagger_view(title = 'API')
 
 urlpatterns = [
     path('docs/', swagger_view),
 ]
+urlpatterns += router.urls
--- universe_main/api_v1/views.py
+++ universe_main/api_v1/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
+from rest_framework.viewsets import ModelViewSet
+
+from api_v1.serializers import UserSerializer
 # Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
