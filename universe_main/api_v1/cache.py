fd4304ccd3f06afc92c7b481770b34e564fe17f8
Voldemat <77781574+Voldemat@users.noreply.github.com>
2021-04-28 12:10:11 +0300

    'override_default_set_and_get_methods_redis'

--- universe_main/api_v1/cache.py
+++ universe_main/api_v1/cache.py
@@ -1,31 +1,65 @@
 import redis
 import json
 
+from typing import Optional, Union
+
+
 from django.conf import settings
 
 from api_v1.serializers import UserSerializer
 
 
-redis_instance = redis.StrictRedis(
+# redis_instance = redis.StrictRedis(
+#     host = settings.REDIS_HOST,
+#     port = settings.REDIS_PORT,
+#     db = 0
+# )
+
+# def set_user_model(obj:object) -> tuple:
+#     # Parse obj into json
+#     obj_json = UserSerializer(obj, many = False).data
+
+#     # create redis_object_prototype = (id, json_content)
+#     redis_object_instance = {
+#         'name'   :   f"users_user-{obj_json['id']}",
+#         'value' :   str(obj_json)
+#     }
+
+#     # set object in database
+#     redis_instance.set(**redis_object_instance)
+
+#     # return representation of redis object
+#     return redis_object_instance['value']
+
+# redis_instance.set_user_model = set_user_model
+
+
+class Redis(redis.StrictRedis):
+    def get(self, name:str, **kwargs:dict) -> Optional[str]:
+        if 'prefix' in kwargs:
+            name = kwargs['prefix'] + ':' + str(name)
+
+        try:
+            value = super().get(name).decode('UTF-8')
+        except AttributeError:
+            return None
+
+        if 'json' in kwargs:
+            value = json.loads(value)
+
+        return value
+
+    def set(self, name:str, value:Union[str, dict], **kwargs:dict) -> None:
+        if 'json' in kwargs:
+            value = str(value)
+
+        if 'prefix' in kwargs:
+            name = kwargs['prefix'] + ':' + str(name)
+        super().set(name, value)
+        return None
+
+redis_db = Redis(
     host = settings.REDIS_HOST,
     port = settings.REDIS_PORT,
     db = 0
 )
-
-def set_user_model(obj:object) -> tuple:
-    # Parse obj into json
-    obj_json = UserSerializer(obj, many = False).data
-
-    # create redis_object_prototype = (id, json_content)
-    redis_object_instance = {
-        'name'   :   f"users_user-{obj_json['id']}",
-        'value' :   str(obj_json)
-    }
-
-    # set object in database
-    redis_instance.set(**redis_object_instance)
-
-    # return representation of redis object
-    return redis_object_instance['value']
-
-redis_instance.set_user_model = set_user_model
--- universe_main/api_v1/views.py
+++ universe_main/api_v1/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.viewsets import ModelViewSet
 
-from api_v1.cache import redis_instance as redis
+from api_v1.cache import redis_db as redis
 from api_v1.serializers import UserSerializer
 from api_v1.permissions import UserObjOrReadOnly
 
@@ -28,13 +28,13 @@ def cache_api(request):
         if not user_id:
             return JsonResponse({'error':"Id is not defined"}, status = 400)
 
-        if redis.get(f'users_user-{user_id}'):
-            user = redis.get(f'users_user-{user_id}')
+        if redis.get(user_id, prefix = 'users_user'):
+            user = redis.get(user_id, prefix = 'users_user')
             print('redis',user)
-            return JsonResponse({str(user_id):user.decode('UTF-8')}, status = 200)
+            return JsonResponse({str(user_id):user}, status = 200)
         else:
             user = get_user_model().objects.get(id = user_id)
-            redis.set_user_model(user)
+            redis.set(name = user_id, value = UserSerializer(user).data, json = True, prefix = 'users_user')
             print('django', user)
             return JsonResponse({str(user.id):user.email}, status = 200)
 
